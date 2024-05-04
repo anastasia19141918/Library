f93a68bdb455b918dbe5e60f97ab694b7496648d
Anastasiya <anastasiya.anast.ru@mail.ru>
2024-05-04 20:17:23 +0300

    feat: add slaider favorite section

---
+++ src/js/summer.js
@@ -0,0 +1,32 @@
+let summer = [
+  {
+    "id": 1,
+    "name": "Crude: A Memoir",
+    "author": "By Pablo Fajardo & ​​Sophie Tardy-Joubert",
+    "img": "./src/img/Summer/By Pablo Fajardo & ​​Sophie Tardy-Joubert.jpg",
+    "description": "Drawing and color by Damien Roudeau | This book illustrates the struggles of a group of indigenous Ecuadoreans as they try to sue the ChevronTexaco company for damage their oil fields did to the Amazon and her people"
+  },
+  {
+    "id": 2,
+    "name": "Let My People Go Surfing",
+    "author": "By Yvon Chouinard",
+    "img": "./src/img/Summer/By Yvon Chouinard.jpg",
+    "description": "Chouinard—climber, businessman, environmentalist—shares tales of courage and persistence from his experience of founding and leading Patagonia, Inc. Full title: Let My People Go Surfing: The Education of a Reluctant Businessman, Including 10 More Years of Business Unusual."
+  },
+  {
+    "id": 3,
+    "name": "The Octopus Museum: Poems",
+    "author": "By Brenda Shaughnessy",
+    "img": "./src/img/Summer/By Brenda Shaughnessy.jpg",
+    "description": "This collection of bold and scathingly beautiful feminist poems imagines what comes after our current age of environmental destruction, racism, sexism, and divisive politics."
+  },
+  {
+    "id": 4,
+    "name": "Shark Dialogues: A Novel",
+    "author": "By Kiana Davenport",
+    "img": "./src/img/Summer/By Kiana Davenport.jpg",
+    "description": "An epic saga of seven generations of one family encompasses the tumultuous history of Hawaii as a Hawaiian woman gathers her four granddaughters together in an erotic tale of villains and dreamers, queens and revolutionaries, lepers and healers."
+  }
+]
+
+export default summer;
