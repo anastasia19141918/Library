6013ee30a5a8914d4e4e6f634a0b7f9a5b556176
Anastasiya <anastasiya.anast.ru@mail.ru>
2024-05-05 13:22:15 +0300

    feat: add section about btn prev and nex

---
+++ src/js/slaiders.js
@@ -0,0 +1,20 @@
+const slaiders = document.getElementById('about__slaiders');
+const slaider = Array.from(slaiders.querySelectorAll('.about__slaid'));
+const slaiderCount = slaider.length;
+
+let slaiderIndex = 0;
+
+
+//btn
+const btnOne = document.getElementById('about__btn_one');
+const btnTwo = document.getElementById('about__btn_two');
+const btnThree = document.getElementById('about__btn_three');
+
+//btn media
+
+const btnFour = document.getElementById('about__btn_four');
+const btnFive = document.getElementById('about__btn_five');
+
+
+
+
