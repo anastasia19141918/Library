8bddf44928a4221243961224940c7e6a2a40b158
Anastasiya <anastasiya.anast.ru@mail.ru>
2024-05-07 18:05:25 +0300

    feat: add favorite slaider

--- src/js/slaiders.js
+++ src/js/slaiders.js
@@ -1,19 +1,122 @@
-const slaiders = document.getElementById('about__slaiders');
+const slaideImg = document.getElementById('about__slaid');
+
+const slaiders = document.getElementById('slaiders');
 const slaider = Array.from(slaiders.querySelectorAll('.about__slaid'));
 const slaiderCount = slaider.length;
 
+
 let slaiderIndex = 0;
 
-
 //btn
-const btnOne = document.getElementById('about__btn_one');
-const btnTwo = document.getElementById('about__btn_two');
-const btnThree = document.getElementById('about__btn_three');
+const dots = document.querySelectorAll('.about__btn');
+const dot = document.getElementById('about__paginations');
 
-//btn media
+const btnOne = document.getElementById('about__btn_one'),
+      btnTwo = document.getElementById('about__btn_two'),
+      btnThree = document.getElementById('about__btn_three'),
+      btnFour = document.getElementById('about__btn_four'),//btn media
+      btnFive = document.getElementById('about__btn_five');//btn media
 
-const btnFour = document.getElementById('about__btn_four');
-const btnFive = document.getElementById('about__btn_five');
+dot.addEventListener('click', menuDots);
+
+function menuDots(el) {
+  let click = el.target;
+
+  if(click.classList.contains('about__btn')) {
+    dotsActive (click);
+  }
+}
+
+function dotsActive (el) {
+  dots.forEach(function(e){
+  e.classList.remove('about__activ');
+})
+ el.classList.add('about__activ');
+} 
+
+// slaider start
+
+function slaid () {
+  const imgWidth = '480';
+  const slideOffset = -slaiderIndex * imgWidth;
+  slaiders.style.transform = `translateX(${slideOffset}px)`;
+}
+
+// slaider end
+
+btnOne.addEventListener('click', function(e){
+  const imgWidth = '480';
+  const slideOffset = -slaiderIndex * imgWidth;
+  if(slideOffset === -0 || slideOffset === -960 || slideOffset === -480 || slideOffset === -1440 || slideOffset === -1920) {
+      slaiders.style.transform = `translateX(0px)`;
+    }
+
+    if(e.target.classList.contains('about__activ')) {
+    return;
+  }
+})
+
+btnTwo.addEventListener('click', function(e){
+  const imgWidth = '480';
+  const slideOffset = -slaiderIndex * imgWidth;
+  
+  if(slideOffset === 0 || slideOffset === -480 || slideOffset === -960) {
+    slaiders.style.transform = `translateX(-480px)`;
+  }
+
+  if(e.target.classList.contains('about__activ')) {
+    return;
+  }
+})
+
+btnThree.addEventListener('click', function(e){
+  const imgWidth = '480';
+  const slideOffset = -slaiderIndex * imgWidth;
+  if(slideOffset === -0 || slideOffset === -960 || slideOffset === -480 || slideOffset === -1440 || slideOffset === -1920) {
+      slaiders.style.transform = `translateX(-960px)`;
+    }
+  
+    if(e.target.classList.contains('about__activ')) {
+    return;
+  }
+})
+
+//media slaiders
+
+btnFour.addEventListener('click', function(e){
+  const imgWidth = '480';
+  const slideOffset = -slaiderIndex * imgWidth;
+
+  if(slideOffset === -0) {
+      slaiders.style.transform = `translateX(-1440px)`;
+    }
+   
+  if(e.target.classList.contains('about__activ')) {
+    return;
+  }
+})
+
+btnFive.addEventListener('click', function(e){
+  const imgWidth = '480';
+  const slideOffset = -slaiderIndex * imgWidth;
+
+  if(slideOffset === -0) {
+    slaiders.style.transform = `translateX(-1920px)`;
+  }
+ 
+ if(e.target.classList.contains('about__activ')) {
+  return;
+}
+})
+
+window.addEventListener('load', () => {
+  slaid();
+});
+
+
+
+
+
 
 
 
