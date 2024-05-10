a1295aaa8a9ecc7eb11375d168fdad171f9db97c
Anastasiya <anastasiya.anast.ru@mail.ru>
2024-05-10 15:21:41 +0300

    feat: add text valid

--- src/js/register.js
+++ src/js/register.js
@@ -9,6 +9,9 @@ const modalRegistrClose = document.getElementById('modal-register_closed');
 const btnEye = document.getElementById('input-register-password');
 const eye = document.getElementById('input-register-password-control');
 
+const btnRegistr = document.getElementById('modal-register__btn');
+const notValid = document.getElementById('modal-register__notValid');
+
 registration.addEventListener('click', function(){
   modalRegistr.classList.add('modal-register__active');
 })
@@ -26,6 +29,10 @@ document.addEventListener('mouseup', function(e){
   if(!modalBody.contains(e.target)) {
     modalRegistr.classList.remove('modal-register__active');
   }
+
+  if(!notValid.contains(e.target)) {
+    notValid.classList.remove('modal-register__notValid__active');
+  }
 })
 
 function showPassword() {
@@ -37,4 +44,22 @@ function showPassword() {
     btnEye.setAttribute('type', 'password');
   }
    return false;
+}
+
+btnRegistr.addEventListener('click', registrBtn);
+
+function registrBtn(e) {
+  const firstName = document.getElementById('input-register_name').value;
+  const lastName = document.getElementById('input-register_nameLast').value;
+  const email = document.getElementById('input-register_email').value;
+  const password = document.getElementById('input-register-password').value;
+
+  e.preventDefault();
+
+  if(firstName !== '' && lastName !== '' && email !== '' && password !== '') {
+    console.log('ne пусто');
+  }
+  else {
+    notValid.classList.add('modal-register__notValid__active');
+  }
 }
